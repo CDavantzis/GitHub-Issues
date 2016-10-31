c3a7656024fba2d7dae12aa98aaaeb1caac5730d
Constantine Davantzis <c.davantzis@gmail.com>
2016-10-31 18:48:26 -0400

    Add "TODO" comments

--- git_hub_issues.py
+++ git_hub_issues.py
@@ -5,5 +5,6 @@ def get_repository_issues(owner, repo):
 	path = "https://api.github.com/repos/{0}/{1}/issues".format(owner, repo)
 	return requests.get(path)
 
-
+# TODO: save data from each page for URL
+# TODO: Check for rate limit
 print get_repository_issues("rg3", "youtube-dl").headers
