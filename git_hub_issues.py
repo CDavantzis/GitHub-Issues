b2c678ead79b9b800bba8406c812ea49793e2f0e
Constantine Davantzis <c.davantzis@gmail.com>
2016-11-04 14:27:13 -0400

    Change tabs to spaces

--- git_hub_issues.py
+++ git_hub_issues.py
@@ -1,9 +1,10 @@
-import requests  #  http://docs.python-requests.org/en/master/
+import requests  # http://docs.python-requests.org/en/master/
 
 
 def get_repository_issues(owner, repo):
-	path = "https://api.github.com/repos/{0}/{1}/issues".format(owner, repo)
-	return requests.get(path)
+    path = "https://api.github.com/repos/{0}/{1}/issues".format(owner, repo)
+    return requests.get(path)
+
 
 # TODO: save data from each page for URL
 # TODO: Check for rate limit
