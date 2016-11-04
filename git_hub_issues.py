7ed1d4b7a3c6de63789290e9da79d7b847212e87
Constantine Davantzis <c.davantzis@gmail.com>
2016-11-04 14:46:52 -0400

    Define GitHub Repo Class

--- git_hub_issues.py
+++ git_hub_issues.py
@@ -2,11 +2,22 @@ import requests  # http://docs.python-requests.org/en/master/
 import json
 
 
-def get_repository_issues(owner, repo, per_page=100):
-    path = "https://api.github.com/repos/{0}/{1}/issues".format(owner, repo)
-    return requests.get(path, params={"per_page": per_page})
+class Repo(object):
+
+    def __init__(self, owner_name, repo_name):
+        self.owner_name = owner_name
+        self.repo_name = repo_name
+        self.path = {"issues": "https://api.github.com/repos/{0}/{1}/issues".format(owner_name, repo_name)}
+
+    def get_issues(self, per_page=100):
+        return requests.get(self.path["issues"], params={"per_page": per_page})
 
 
-# TODO: save data from each page for URL
-# TODO: Check for rate limit
-r = get_repository_issues("rg3", "youtube-dl")
+if __name__ == '__main__':
+    # TODO: save data from each page for URL
+    # TODO: Check for rate limit
+    repo = Repo("rg3", "youtube-dl")
+    r = repo.get_issues()
+    print json.dumps(r.json(), sort_keys=True, indent=4, separators=(',', ': '))
+
+    print repo.repo_name
