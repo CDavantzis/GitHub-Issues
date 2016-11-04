4f637d3a30637a0ffc13caf06ae89496d0270808
Constantine Davantzis <c.davantzis@gmail.com>
2016-11-04 15:47:23 -0400

    Initial Commit For "update_issues"

--- git_hub_issues.py
+++ git_hub_issues.py
@@ -8,16 +8,32 @@ class Repo(object):
         self.owner_name = owner_name
         self.repo_name = repo_name
         self.path = {"issues": "https://api.github.com/repos/{0}/{1}/issues".format(owner_name, repo_name)}
+        self.issues = []
 
-    def get_issues(self, per_page=100):
-        return requests.get(self.path["issues"], params={"per_page": per_page})
+    def update_issues(self, starting_page=1):
+        r = requests.get(self.path["issues"], params={"per_page": 100, "page": starting_page})
+
+        if r.headers.get("X-RateLimit-Remaining") == "0":
+            print "WARNING: Rate Limit Exceeded"
+            return self.issues
+
+        self.issues += r.json()
+        while r.links.get("next") is not None:
+            r = requests.get(r.links["next"]["url"])
+            self.issues += r.json()
+
+        if r.headers.get("X-RateLimit-Remaining") == "0":
+            print "WARNING: Rate Limit Exceeded"
+            return self.issues
+
+        self.issues += r.json()
+        return self.issues
 
 
 if __name__ == '__main__':
     # TODO: save data from each page for URL
     # TODO: Check for rate limit
     repo = Repo("rg3", "youtube-dl")
-    r = repo.get_issues()
-    print json.dumps(r.json(), sort_keys=True, indent=4, separators=(',', ': '))
+    r = repo.update_issues()
+    print r
 
-    print repo.repo_name
