5021e3fb942c775dbc7a5d2cfbf0337ff4535d41
Constantine Davantzis <c.davantzis@gmail.com>
2016-11-04 14:35:59 -0400

    get 100 repo issues per api request
    ** Helps deal with github rate limit

--- git_hub_issues.py
+++ git_hub_issues.py
@@ -1,11 +1,12 @@
 import requests  # http://docs.python-requests.org/en/master/
+import json
 
 
-def get_repository_issues(owner, repo):
+def get_repository_issues(owner, repo, per_page=100):
     path = "https://api.github.com/repos/{0}/{1}/issues".format(owner, repo)
-    return requests.get(path)
+    return requests.get(path, params={"per_page": per_page})
 
 
 # TODO: save data from each page for URL
 # TODO: Check for rate limit
-print get_repository_issues("rg3", "youtube-dl").headers
+r = get_repository_issues("rg3", "youtube-dl")
