0ca7b3512a80e976b29ef0ab3a1b2b77114c674f
Tyranicangel <prad.ads1990@gmail.com>
2016-11-06 15:47:53 -0500

    Added more metrics

--- analyze.py
+++ analyze.py
@@ -1,5 +1,6 @@
 import json
 from collections import defaultdict
+from datetime import datetime
 
 
 class Issues(object):
@@ -21,8 +22,48 @@ class Issues(object):
             contributors[issue["user"]["login"]][issue["state"]] += 1
         return dict(contributors)
 
+    @property
+    def number_of_issues_assigned_to_individual(self):
+        assingees=defaultdict(lambda:{'name':'',"number":0})
+        for issue in self.json:
+            if len(issue['assignees'])>0:
+                for assignee in issue['assignees']:
+                    assignees[assignee['id']]['name']=assignee['login']
+                    assignees[assignee['id']]['number']+=1
+        return dict(assignees)
+
+    @property 
+    def time_taken_for_closing_issue(self):
+        closed_issues={}
+        for issue in self.json:
+            if issue['state']=='closed':
+                closed_issues[issue['id']]=abs((datetime.strptime(issue['closed_at'],'%Y-%m-%dT%H:%M:%SZ')-datetime.strptime(issue['created_at'],'%Y-%m-%dT%H:%M:%SZ')).days)
+        return closed_issues
+
+    @property
+    def issues_closed_per_milestone(self):
+        milestones={}
+        for issue in self.json:
+            if type(issue['milestone']) is dict and not issue['milestone']['id'] in milestones:
+                milestones[issue['milestone']['id']]={'title':issue['milestone']['title'],'closed':issue['milestone']['closed_issues'],'open':issue['milestone']['open_issues']}
+        return milestones
+
+    @property
+    def issues_per_tag(self):
+        labels=defaultdict(lambda:{"name":"","counter":0})
+        for issue in self.json:
+             if len(issue['labels'])>0:
+                for label in issue['labels']:
+                    labels[label['id']]['name']=label['name']
+                    labels[label['id']]['counter']+=1
+        return dict(labels)
+
 
 if __name__ == '__main__':
-    i = Issues('data/MediaBrowser_Emby_issues_1478411769.json')
-    print i.number_of_comments_per_issue
-    print i.number_of_issues_raised_per_contributor
+    i = Issues('data/angular_angular_issues_1478462251.json')
+    # print i.number_of_comments_per_issue
+    # print i.number_of_issues_raised_per_contributor
+    # print i.time_taken_for_closing_issue
+    # print i.issues_closed_per_milestone
+    # print i.issues_per_tag
+    # print i.number_of_issues_assigned_to_individual
