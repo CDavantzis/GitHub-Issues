4b1069b62ea389eafa1879370d2b5645c8a6eb99
Constantine Davantzis <c.davantzis@gmail.com>
2016-11-21 21:41:07 -0500

    Linting

--- analyze.py
+++ analyze.py
@@ -8,25 +8,25 @@ import matplotlib.pyplot as plt
 
 class Issues(object):
     def __init__(self, path):
-        self.fname=path.split('.')[0]
+        self.fname = path.split('.')[0]
         with open(path) as data_file:
             self.json = json.load(data_file)
 
-    def histo(self,data,fname,xaxis,title):
+    def histo(self, data, fname, xaxis, title):
         plt.figure()
-        plt.hist(sorted(data),bins=30)
+        plt.hist(sorted(data), bins=30)
         plt.xlabel(xaxis)
         plt.ylabel('frequency')
         plt.title(title)
-        plt.savefig('graphs/'+fname+'.png')
+        plt.savefig('graphs/' + fname + '.png')
 
     @property
-    def number_of_comments_per_issue(self): 
+    def number_of_comments_per_issue(self):
         issues = {"closed": [], "open": []}
         for issue in self.json:
             issues[issue["state"]].append(issue["comments"])
-        self.histo(issues['open'],'open_issues','comments/issue','Open Issues')
-        self.histo(issues['closed'],'closed_issues','comments/issue','Closed Issues')
+        self.histo(issues['open'], 'open_issues', 'comments/issue', 'Open Issues')
+        self.histo(issues['closed'], 'closed_issues', 'comments/issue', 'Closed Issues')
         return issues
 
     @property
@@ -34,48 +34,55 @@ class Issues(object):
         contributors = defaultdict(lambda: {"open": 0, "closed": 0})
         for issue in self.json:
             contributors[issue["user"]["login"]][issue["state"]] += 1
-        self.histo([contributors[cont]['closed'] for cont in dict(contributors)],'issues_raised','issue raised/contributor','Issue/Contributor')
+        self.histo([contributors[cont]['closed'] for cont in dict(contributors)], 'issues_raised',
+                   'issue raised/contributor', 'Issue/Contributor')
         return dict(contributors)
 
     @property
     def number_of_issues_assigned_to_individual(self):
-        assignees=defaultdict(lambda:{'name':'',"number":0})
+        assignees = defaultdict(lambda: {'name': '', "number": 0})
         for issue in self.json:
-            if len(issue['assignees'])>0:
+            if len(issue['assignees']) > 0:
                 for assignee in issue['assignees']:
-                    assignees[assignee['id']]['name']=assignee['login']
-                    assignees[assignee['id']]['number']+=1
-        self.histo([assignees[assignee]['number'] for assignee in dict(assignees)],'issues_assigned','issue assigned/Person','Issue/Person')
+                    assignees[assignee['id']]['name'] = assignee['login']
+                    assignees[assignee['id']]['number'] += 1
+        self.histo([assignees[assignee]['number'] for assignee in dict(assignees)], 'issues_assigned',
+                   'issue assigned/Person', 'Issue/Person')
         return dict(assignees)
 
-    @property 
+    @property
     def time_taken_for_closing_issue(self):
-        closed_issues={}
+        closed_issues = {}
         for issue in self.json:
-            if issue['state']=='closed':
-                closed_issues[issue['id']]=abs((datetime.strptime(issue['closed_at'],'%Y-%m-%dT%H:%M:%SZ')-datetime.strptime(issue['created_at'],'%Y-%m-%dT%H:%M:%SZ')).days)
-        self.histo([closed_issues[ids] for ids in closed_issues],'time_for_closing','timetaken/issue','Time taken to Close')
+            if issue['state'] == 'closed':
+                closed_issues[issue['id']] = abs((datetime.strptime(issue['closed_at'],
+                                                                    '%Y-%m-%dT%H:%M:%SZ') - datetime.strptime(
+                    issue['created_at'], '%Y-%m-%dT%H:%M:%SZ')).days)
+        self.histo([closed_issues[ids] for ids in closed_issues], 'time_for_closing', 'timetaken/issue',
+                   'Time taken to Close')
         return closed_issues
 
     @property
     def issues_closed_per_milestone(self):
-        milestones={}
+        milestones = {}
         for issue in self.json:
             if type(issue['milestone']) is dict and not issue['milestone']['id'] in milestones:
-                milestones[issue['milestone']['id']]={'name':issue['milestone']['title'],'closed':issue['milestone']['closed_issues'],'open':issue['milestone']['open_issues']}
-        self.histo([milestones[milestone]['closed'] for milestone in milestones],'issues_closed_milestone','issues/milestone','Issues/Milestone')
+                milestones[issue['milestone']['id']] = {'name': issue['milestone']['title'],
+                                                        'closed': issue['milestone']['closed_issues'],
+                                                        'open': issue['milestone']['open_issues']}
+        self.histo([milestones[milestone]['closed'] for milestone in milestones], 'issues_closed_milestone',
+                   'issues/milestone', 'Issues/Milestone')
         return milestones
-
 
     @property
     def issues_per_tag(self):
-        labels=defaultdict(lambda:{"name":"","counter":0})
+        labels = defaultdict(lambda: {"name": "", "counter": 0})
         for issue in self.json:
-             if len(issue['labels'])>0:
+            if len(issue['labels']) > 0:
                 for label in issue['labels']:
-                    labels[label['id']]['name']=label['name']
-                    labels[label['id']]['counter']+=1
-        self.histo([labels[label]['counter'] for label in dict(labels)],'issues_per_tag','issues/tags','Issues/Tag')
+                    labels[label['id']]['name'] = label['name']
+                    labels[label['id']]['counter'] += 1
+        self.histo([labels[label]['counter'] for label in dict(labels)], 'issues_per_tag', 'issues/tags', 'Issues/Tag')
         return dict(labels)
 
     @property
@@ -97,7 +104,7 @@ class Issues(object):
     @property
     def issues_overtime(self):
         date_min, date_max = self.date_range
-        days = (date_max-date_min).days
+        days = (date_max - date_min).days
         issues = [{"open": 0, "closed": 0} for x in range(days)]
 
         for issue in self.json:
@@ -118,12 +125,13 @@ class Issues(object):
 
         return {"date_min": date_min.strftime('%Y-%m-%d'), "date_max": date_max.strftime('%Y-%m-%d'), "results": issues}
 
+
 if __name__ == '__main__':
     i = Issues('data/MediaBrowser_Emby_issues_1478411769.json')
-    #print i.number_of_comments_per_issue
-    #print i.number_of_issues_raised_per_contributor
-    #print i.time_taken_for_closing_issue
-    #print i.issues_closed_per_milestone
-    #print i.issues_per_tag
-    #print i.number_of_issues_assigned_to_individual
+    # print i.number_of_comments_per_issue
+    # print i.number_of_issues_raised_per_contributor
+    # print i.time_taken_for_closing_issue
+    # print i.issues_closed_per_milestone
+    # print i.issues_per_tag
+    # print i.number_of_issues_assigned_to_individual
     print i.issues_overtime
